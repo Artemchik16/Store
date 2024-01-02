6342b383f9ba26ac97930518fdb9cc55a0d25457
Артем Захарченко <artemzaharcenko310@gmail.com>
2024-01-02 20:09:26 +0600

    + & - in cart

--- carts/admin.py
+++ carts/admin.py
@@ -5,7 +5,7 @@ from carts.models import Cart
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    list_display = ['product', 'quantity', 'created_timestamp', 'user']
-    readonly_fields = ['product', 'quantity', 'user']
-    list_filter = ['product__category__name']
-    search_fields = ['product__category__name', 'product__name']
+    list_display = ["product", "quantity", "created_timestamp", "user"]
+    readonly_fields = ["product", "quantity", "user"]
+    list_filter = ["product__category__name"]
+    search_fields = ["product__category__name", "product__name"]
--- carts/views.py
+++ carts/views.py
@@ -1,15 +1,14 @@
 from django.http import JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
 
 from carts.models import Cart
 from carts.utils import get_user_carts
-
 from goods.models import Products
 
 
 def cart_add(request):
-    product_id = request.POST.get('product_id')
+    product_id = request.POST.get("product_id")
     product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
@@ -24,34 +23,52 @@ def cart_add(request):
 
     user_cart = get_user_carts(request)
     cart_items_html = render_to_string(
-        'carts/includes/included_cart.html', {'carts': user_cart}, request=request
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request
     )
 
     response_data = {
-        'message': 'Добавлено!',
-        'cart_items_html': cart_items_html,
+        "message": "Добавлено!",
+        "cart_items_html": cart_items_html,
     }
 
     return JsonResponse(response_data)
 
 
-def cart_change(request, product_slug):
-    pass
+def cart_change(request):
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
+
+    cart = Cart.objects.get(id=cart_id)
+    cart.quantity = quantity
+    cart.save()
+    updated_quantity = cart.quantity
+
+    cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": cart}, request=request
+    )
+
+    response_data = {
+        "message": "Количество измнено",
+        "cart_items_html": cart_items_html,
+        "quantity": updated_quantity,
+    }
+    return JsonResponse(response_data)
 
 
 def cart_remove(request):
-    cart_id = request.POST.get('cart_id')
+    cart_id = request.POST.get("cart_id")
     cart = Cart.objects.get(id=cart_id)
     cart.delete()
 
     user_cart = get_user_carts(request)
     cart_items_html = render_to_string(
-        'carts/includes/included_cart.html', {'carts': user_cart}, request=request
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request
     )
 
     response_data = {
-        'message': 'Товар удален',
-        'cart_items_html': cart_items_html,
-        'quantity_deleted': cart.quantity,
+        "message": "Товар удален",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": cart.quantity,
     }
     return JsonResponse(response_data)
